cf005e067df34a10e6a20ebc11497e7a3642d29b
ergegananputra <126530940+ergegananputra@users.noreply.github.com>
2025-01-18 12:39:43 +0700

    v0.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odd_even_lang_packs",
-    version="0.7",
+    version="0.8",
     packages=find_packages(),
     install_requires=[
         
